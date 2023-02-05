bbeb7f08e0a93f94f6ac30ebd61894c95cc614d0
ch0c0-msk <temerfromkazan@mail.ru>
2023-02-05 03:47:32 +0300

    New project

---
+++ src/main/java/com/example/examplecrm/ExampleCrmApplication.java
@@ -0,0 +1,13 @@
+package com.example.examplecrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleCrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleCrmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/examplecrm/ExampleCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.examplecrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleCrmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
