778fefe944b321e547746965518094e8b5d27870
ch0c0-msk <temerfromkazan@mail.ru>
2023-02-16 02:24:08 +0300

    Add authentication

---
+++ src/main/java/com/example/examplecrm/services/CustomUserDetailsService.java
@@ -0,0 +1,18 @@
+package com.example.examplecrm.services;
+
+import com.example.examplecrm.repos.UserRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepo userRepo;
+    @Override
+    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+        return userRepo.findByLogin(login);
+    }
+}
---
+++ src/main/java/com/example/examplecrm/services/UserService.java
@@ -0,0 +1,25 @@
+package com.example.examplecrm.services;
+
+import com.example.examplecrm.models.User;
+import com.example.examplecrm.models.enums.Role;
+import com.example.examplecrm.repos.UserRepo;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class UserService  {
+    private final UserRepo userRepo;
+
+    public boolean createUser(User user) {
+        String login = user.getLogin();
+        if (userRepo.findByLogin(login) != null) return false;
+        user.setActive(true);
+        user.getRoles().add(Role.ROLE_USER);
+        userRepo.save(user);
+        log.info("Saving new user with login {}",login);
+        return true;
+    }
+}
