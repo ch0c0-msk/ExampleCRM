5e5d61994446c84204e3e637e2f803be1c18e710
ch0c0-msk <temerfromkazan@mail.ru>
2023-02-19 19:08:23 +0300

    Add bootstrap UI

--- src/main/java/com/example/examplecrm/controllers/AdminController.java
+++ src/main/java/com/example/examplecrm/controllers/AdminController.java
@@ -1,18 +1,22 @@
 package com.example.examplecrm.controllers;
 
-import com.example.examplecrm.models.Client;
 import com.example.examplecrm.models.User;
+import com.example.examplecrm.models.enums.Role;
 import com.example.examplecrm.repos.UserRepo;
 import com.example.examplecrm.services.UserService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Optional;
 
 @Controller
 @PreAuthorize("hasAuthority('ADMIN')")
@@ -35,7 +39,7 @@ public class AdminController {
         user.setLogin(login);
         user.setPassword(passwordEncoder.encode(password));
         userService.createUser(user);
-        return "redirect:/login";
+        return "redirect:/users_list";
     }
 
     @GetMapping("users_list")
@@ -43,5 +47,39 @@ public class AdminController {
         Iterable<User> users = userRepo.findAll();
         model.addAttribute("users",users);
         return "users_list";
+    }
+
+    @GetMapping("/edit_user/{id}")
+    public String editUser(@PathVariable(value = "id") Long id, Model model) {
+
+        Optional<User> user = userRepo.findById(id);
+        ArrayList<User> userDetails = new ArrayList<>();
+        user.ifPresent(userDetails::add);
+        model.addAttribute("users",userDetails);
+        model.addAttribute("roles", Role.values());
+        return "edit_user";
+    }
+
+    @PostMapping("/edit_user/{id}")
+    public String modifyUser(@RequestParam("userId") User user, @RequestParam Map<String,String> form, @RequestParam String login) {
+
+        user.setLogin(login);
+        userService.modifyUser(user);
+        userService.modifyUserRoles(user, form);
+        return "redirect:/users_list";
+    }
+
+    @PostMapping("/disable_user/{id}")
+    public String disableUser(@PathVariable(value = "id") Long id) {
+
+        userService.disableUser(id);
+        return "redirect:/users_list";
+    }
+
+    @PostMapping("/enable_user/{id}")
+    public String enableUser(@PathVariable(value = "id") Long id) {
+
+        userService.enableUser(id);
+        return "redirect:/users_list";
     }
 }
